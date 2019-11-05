e174248d10d30bfc0a8ba878930c6acc1322314d
Mason Walker <mwalker0909@gmail.com>
2019-11-05 13:36:38 -0800

    reworked from other classmates in class code review

--- challenges/objects/challenges-06.test.js
+++ challenges/objects/challenges-06.test.js
@@ -96,7 +96,6 @@ const hasChildrenValues = (arr, character) => {
       return true;
     }
   }
-
   return false;
 };
 /* ------------------------------------------------------------------------------------------------
