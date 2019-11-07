a2682a4bc735af9a29dc0891c31226eb75056e44
Mason Walker <mwalker0909@gmail.com>
2019-11-07 14:35:19 -0800

    finished challenge 6

--- challenges/filter/challenges-08.test.js
+++ challenges/filter/challenges-08.test.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import { interfaceDeclaration } from "@babel/types";
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
 
@@ -87,10 +85,10 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  let newArray = arr.filter( element => {
-    return.element.baseStat > minBaseStat;
-  })
-   return newArray;
+  let newArray = arr.filter(element => {
+    return element.baseStat > minBaseStat;
+  });
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -102,7 +100,13 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+  let newArray = arr.filter(element => {
+    return element.baseStat > minBaseStat;
+  });
+  newArray = newArray.map(element => {
+    return element = element.stat.name;
+  });
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -166,9 +170,9 @@ Write a function named evenOddNumericValues that, given an array as input, uses 
 For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 'odd'].
 ------------------------------------------------------------------------------------------------ */
 
-const evenOddNumericValues = (arr) => {
-  // Solution code here...
-};
+//THIS IS TAKEN FROM TRAVIS IN CODE REVIEW. PRIOR HOLLY WAS ATTEMPTING TO SOLVE, AND WE COVERED THIS IN CODE REVIEW FROM TRAVIS
+const evenOddNumericValues = (arr) => arr.filter(item => typeof(item) === 'number').map(num => num % 2 === 0 ? 'even' : 'odd');
+
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
